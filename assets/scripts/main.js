9626f2b9b6abe1cfc0043db1873f2ebdc40a9338
semenov-vldr <semenov-vldr@users.noreply.github.com>
2024-01-31 20:40:20 +0000

    deploy: d086ae8d5f8dca755e77cf8d6aee664f1391bd9a

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -52,6 +52,32 @@ new Swiper('.catalog__slider', {
 });
 "use strict";
 
+new Swiper('.event-header__slider', {
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true
+  },
+  navigation: {
+    prevEl: '.swiper-button-prev',
+    nextEl: '.swiper-button-next'
+  },
+  // autoplay: {
+  //   delay: 3000,
+  //   disableOnInteraction: false,
+  // },
+
+  // Откл функционала, если слайдов меньше, чем нужно
+  watchOverflow: true,
+  slidesPerView: 1,
+  effect: "fade",
+  // Отступ между слайдами
+  spaceBetween: 16,
+  // Стартовый слайд
+  initialSlide: 0,
+  loop: true
+});
+"use strict";
+
 var accordionItems = document.querySelectorAll('.accordion__item'); // список элементов аккордиона
 
 if (accordionItems) {
